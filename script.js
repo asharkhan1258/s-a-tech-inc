8158d066b13519ec346c78962f3d478f560a4b4c
Abdur Rafay <158170390+Rafaykz@users.noreply.github.com>
2026-05-04 18:24:17 +0500

    new update

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ document.querySelectorAll(".saa-fq-question").forEach((btn) => {
 
   });
 });
+
+  // Show popup when page loads
+  window.onload = function() {
+    document.getElementById("popupOverlay").style.display = "flex";
+  };
+
+  function closePopup() {
+    document.getElementById("popupOverlay").style.display = "none";
+  }
